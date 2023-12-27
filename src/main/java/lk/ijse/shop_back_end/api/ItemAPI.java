2327ed4a4751b23f9d644f5fabd94971fc96dcfa
dinildamsith <dinildamsith52@gmail.com>
2023-12-27 19:19:31 +0530

    add item Update function

--- src/main/java/lk/ijse/shop_back_end/api/ItemAPI.java
+++ src/main/java/lk/ijse/shop_back_end/api/ItemAPI.java
@@ -12,10 +12,7 @@ import lk.ijse.shop_back_end.dto.ItemDTO;
 import lombok.SneakyThrows;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 @WebServlet(name = "item",urlPatterns = "/item",
         initParams = {
@@ -53,7 +50,7 @@ public class ItemAPI extends HttpServlet {
 
         Jsonb jsonb = JsonbBuilder.create();
         ItemDTO itemDTO = jsonb.fromJson(req.getReader(),ItemDTO.class);
-        System.out.println(itemDTO);
+
         PreparedStatement preparedStatement = connection.prepareStatement(SAVE_DATA);
         preparedStatement.setString(1,itemDTO.getItemId());
         preparedStatement.setString(2,itemDTO.getItemName());
@@ -70,5 +67,42 @@ public class ItemAPI extends HttpServlet {
 
     }
 
+    String UPDATE_ITEM = "UPDATE Items SET items.itemName = ?,items.itemPrice = ?,items.itemDesc = ? WHERE items.itemId =?";
+    @SneakyThrows
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Jsonb jsonb = JsonbBuilder.create();
+        ItemDTO itemDTO = jsonb.fromJson(req.getReader(),ItemDTO.class);
+        System.out.println(itemDTO);
+        PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_ITEM);
+        preparedStatement.setString(1,itemDTO.getItemName());
+        preparedStatement.setDouble(2,itemDTO.getItemPrice());
+        preparedStatement.setString(3,itemDTO.getItemDesc());
+        preparedStatement.setString(4,itemDTO.getItemId());
 
+        if (preparedStatement.executeUpdate() !=0){
+            System.out.println("Item Update ");
+        }else {
+            System.out.println("Not Update ");
+        }
+    }
+
+
+    //    String SEARCH_DATA = "SELECT * FROM Items WHERE itemId = ? ";
+//    @SneakyThrows
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        Jsonb jsonb = JsonbBuilder.create();
+//        ItemDTO itemDTO = jsonb.fromJson(req.getReader(),ItemDTO.class);
+//        PreparedStatement preparedStatement = connection.prepareStatement(SEARCH_DATA);
+//        preparedStatement.setString(1,itemDTO.getItemId());
+//        ResultSet resultSet = preparedStatement.executeQuery();
+//
+//        while (resultSet.next()){
+//            System.out.println(resultSet.getString(1));
+//            System.out.println(resultSet.getString(2));
+//            System.out.println(resultSet.getString(3));
+//            System.out.println(resultSet.getString(4));
+//        }
+//    }
 }
